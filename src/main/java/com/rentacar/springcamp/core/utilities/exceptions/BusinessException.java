d6bde1531636605710b8264db1039bafca5ff700
gkhnpldkn <gkhnpldkn@gmail.com>
2023-03-03 20:51:56 +0300

    Exception handling improvements.

---
+++ src/main/java/com/rentacar/springcamp/core/utilities/exceptions/BusinessException.java
@@ -0,0 +1,7 @@
+package com.rentacar.springcamp.core.utilities.exceptions;
+
+public class BusinessException extends RuntimeException {
+    public BusinessException(String message) {
+        super(message);
+    }
+}
